d6adde13ee20840d844fe9abeeffca4823c98515
yangyi <15935779184@sina.cn>
2019-11-27 14:09:59 +0800

    第一次

---
+++ src/components/QuestionAndAnswer/content.js
@@ -0,0 +1,27 @@
+import React from "react";
+import './content.css'
+
+export default class Content extends React.Component {
+    render() {
+        return (
+            <content className="s-41d1d672">
+                <div className="s-45b3ab02"><span>精华问答</span></div>
+                <div className="s-45b3ab02 active">
+                    <span>最新问题</span>
+                    <div className="s9b31ec9"></div>
+                </div>
+                <div className="s-45b3ab02"><span>最热问题</span></div>
+            </content>
+        )
+    }
+}
+{/*<div className="s-41d1d672" data-reactid=".inh65lu0p6.1.0.3.0" style="position:fixed;left:0;right:0;z-index:6;">*/}
+{/*<div className="s-45b3ab02" style="width:33.33%;" data-reactid=".inh65lu0p6.1.0.3.0.$questionEssence"><span*/}
+{/*data-reactid=".inh65lu0p6.1.0.3.0.$questionEssence.0">精华问答</span></div>*/}
+{/*<div className="s-45b3ab02 active" style="width:33.33%;" data-reactid=".inh65lu0p6.1.0.3.0.$questionNew">*/}
+{/*<span data-reactid=".inh65lu0p6.1.0.3.0.$questionNew.0">最新问题</span>*/}
+{/*<div className="s9b31ec9" style="width:50px;" data-reactid=".inh65lu0p6.1.0.3.0.$questionNew.1"></div>*/}
+{/*</div>*/}
+{/*<div className="s-45b3ab02" style="width:33.33%;" data-reactid=".inh65lu0p6.1.0.3.0.$questionHot"><span*/}
+{/*data-reactid=".inh65lu0p6.1.0.3.0.$questionHot.0">最热问题</span></div>*/}
+{/*</div>*/}
--- src/components/tarber/index.js
+++ src/components/tarber/index.js
@@ -1,25 +1,26 @@
-import React, { Component } from 'react'
-import { tabBarRoute } from "../../router"
+import React, {Component} from 'react'
+import {tabBarRoute} from "../../router"
 import {
     NavLink,
 } from "react-router-dom"
 import "./tarber.css"
+
 export default class TabBer extends Component {
     render() {
         return (
-              
-                    <ul className="tabBar">
-                        {
-                            tabBarRoute[0].children.map((v, i) => (
-                                    <NavLink exact key={i}  to={v.path} activeClassName="actived">
-                                        <div className="imgCon"> <img src={"/img"+v.imgSrc} alt=""/></div>
-                                        <div>{v.name}</div>
-                                    </NavLink>
-                              
-                            ))
-                        }
-                    </ul>
-               
-        )  
+
+            <ul className="tabBar">
+                {
+                    tabBarRoute[0].children.map((v, i) => (
+                        <NavLink exact key={i} to={v.path} activeClassName="actived">
+                            <div className="imgCon"><img src={"/img" + v.imgSrc} alt=""/></div>
+                            <div>{v.name}</div>
+                        </NavLink>
+
+                    ))
+                }
+            </ul>
+
+        )
     }
 }
