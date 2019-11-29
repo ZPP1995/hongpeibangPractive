c8e566baf2027912fff7a6c06a73141e08ea002c
yangyi <15935779184@sina.cn>
2019-11-29 21:27:07 +0800

    渲染

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {
-  Route,
-  Switch,
-  BrowserRouter as Router
+    Route,
+    Switch,
+    BrowserRouter as Router
 } from "react-router-dom"
 import Home from "./views/home"
 import {noTabBarRoute} from "./router"
@@ -14,27 +14,28 @@ import './App.css';
 
 class App extends React.Component {
 
-  render() {
-    return (
-      <Router>
-      <div className="App">
-        <Switch>
-         
-        {
-          noTabBarRoute.map((v,i)=>(
-            <Route key={i} path={v.path} exact component={v.component}></Route>
-          ))
-        }
-        <Route path={"/"}  component={Home}></Route>
-        </Switch>
-      </div>
-      </Router>
-    );
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <Switch>
 
-  }
-  componentDidMount(){
-    
-  }
+                        {
+                            noTabBarRoute.map((v, i) => (
+                                <Route key={i} path={v.path} exact component={v.component}></Route>
+                            ))
+                        }
+                        <Route path={"/"} component={Home}></Route>
+                    </Switch>
+                </div>
+            </Router>
+        );
+
+    }
+
+    componentDidMount() {
+
+    }
 
 }
 
